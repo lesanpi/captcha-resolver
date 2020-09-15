171530b5c1a99d8b9d49863a7efd198f1083780a
Luis Sanchez <lespinerua@gmail.com>
2020-09-15 03:08:30 -0400

    Discriminador y Generador

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ SOURCE_PATH = 'source'  # 'data/source/jpg'
 TARGET_PATH = 'target'  # 'data/target/jpg'
 
 # SIZE (200, 50)
-IMG_HEIGHT = 50
-IMG_WIDTH = 200
+IMG_HEIGHT = 256
+IMG_WIDTH = 256
 
 # Imagenes en la carpeta
 imagesJPG = [f for r, d, f in os.walk(SOURCE_PATH)]
 
 
+@tf.function()
 def resize(inimg, tgimg, height, width):
 
     # inimg = tf.image.resize(
@@ -152,3 +153,98 @@ def upsample(filters, apply_dropout=False):
     result.add(tf.keras.layers.ReLU())
 
     return result
+
+
+def Generator():
+    inputs = tf.keras.layers.Input(shape=[256, 256, 3])
+
+    down_stack = [
+        downsample(64, apply_batchnorm=False),  # (bs, 128, 128, 64)
+        downsample(128),  # (bs, 64, 64, 128)
+        downsample(256),  # (bs, 32, 32, 256)
+        downsample(512),  # (bs, 16, 16, 512)
+        downsample(512),  # (bs, 8, 8, 512)
+        downsample(512),  # (bs, 4, 4, 512)
+        downsample(512),  # (bs, 2, 2, 512)
+        downsample(512),  # (bs, 1, 1, 512)
+    ]
+
+    up_stack = [
+        upsample(512, apply_dropout=True),  # (bs, 2, 2, 1024)
+        upsample(512, apply_dropout=True),  # (bs, 4, 4, 1024)
+        upsample(512, apply_dropout=True),  # (bs, 8, 8, 1024)
+        upsample(512),  # (bs, 16, 16, 1024)
+        upsample(256),  # (bs, 32, 32, 512)
+        upsample(128),  # (bs, 64, 64, 256)
+        upsample(64),  # (bs, 128, 128, 128)
+    ]
+
+    initializer = tf.random_normal_initializer(0., 0.02)
+    last = tf.keras.layers.Conv2DTranspose(3, 4,
+                                           strides=2,
+                                           padding='same',
+                                           kernel_initializer=initializer,
+                                           activation='tanh')  # (bs, 256, 256, 3)
+
+    x = inputs
+
+    # Downsampling a traves del modelo
+    skips = []
+    for down in down_stack:
+        x = down(x)
+        skips.append(x)
+
+    skips = reversed(skips[:-1])
+
+    # Upsampling y uniendo las skips connections
+    for up, skip in zip(up_stack, skips):
+        x = up(x)
+        x = tf.keras.layers.Concatenate()([x, skip])
+
+    x = last(x)
+
+    return tf.keras.Model(inputs=inputs, outputs=x)
+
+
+# Ejemplo
+inp, re = load_train_image(train_images[0])
+inp = (inp + 1) * 127.5
+re = (re + 1) * 127.5
+generator = Generator()
+# gen_out = generator(inp[tf.newaxis, ...], training=False)
+
+# plt.imshow(re / 255)
+# plt.show()
+
+
+def Discriminator():
+    initializer = tf.random_normal_initializer(0., 0.02)
+
+    # Input y Target
+    inp = tf.keras.layers.Input(shape=[256, 256, 3], name='input_image')
+    tar = tf.keras.layers.Input(shape=[256, 256, 3], name='target_image')
+
+    x = tf.keras.layers.concatenate([inp, tar])  # (bs, 256, 256, channels*2)
+
+    down1 = downsample(64, False)(x)  # (bs, 128, 128, 64)
+    down2 = downsample(128)(down1)  # (bs, 64, 64, 128)
+    down3 = downsample(256)(down2)  # (bs, 32, 32, 256)
+    down4 = downsample(256)(down3)
+
+    # zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3)  # (bs, 34, 34, 256)
+    # conv = tf.keras.layers.Conv2D(512, 4, strides=1,
+    #                              kernel_initializer=initializer,
+    #                              use_bias=False)(zero_pad1)  # (bs, 31, 31, 512)
+
+    #batchnorm1 = tf.keras.layers.BatchNormalization()(conv)
+
+    #leaky_relu = tf.keras.layers.LeakyReLU()(batchnorm1)
+
+    # zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu)  # (bs, 33, 33, 512)
+
+    # last = tf.keras.layers.Conv2D(1, 4, strides=1,
+    #                              kernel_initializer=initializer)(zero_pad2)  # (bs, 30, 30, 1)
+    last = tf.keras.layers.Conv2D(1, 4, strides=1,
+                                  kernel_initializer=initializer)(down4)  # (bs, 30, 30, 1)
+
+    return tf.keras.Model(inputs=[inp, tar], outputs=last)
