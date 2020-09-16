72ab5b2b8966ca6969fb51edb2caf35c3e882cc9
Luis Sanchez <lespinerua@gmail.com>
2020-09-15 22:59:30 -0400

    Training and train steps

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from IPython import display
+import time
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,10 +22,10 @@ imagesJPG = [f for r, d, f in os.walk(SOURCE_PATH)]
 @tf.function()
 def resize(inimg, tgimg, height, width):
 
-    # inimg = tf.image.resize(
-    #    inimg, [int(height), int(width)])
-    # tgimg = tf.image.resize(
-    #    tgimg, [int(height), int(width)])
+    inimg = tf.image.resize(
+        inimg, [int(height), int(width)])
+    tgimg = tf.image.resize(
+        tgimg, [int(height), int(width)])
 
     return inimg, tgimg
 
@@ -248,3 +250,112 @@ def Discriminator():
                                   kernel_initializer=initializer)(down4)  # (bs, 30, 30, 1)
 
     return tf.keras.Model(inputs=[inp, tar], outputs=last)
+
+
+discriminator = Discriminator()
+
+loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+LAMBDA = 100
+
+
+def generator_loss(disc_generated_output, gen_output, target):
+    gan_loss = loss_object(tf.ones_like(
+        disc_generated_output), disc_generated_output)
+
+    # mean absolute error
+    l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
+
+    total_gen_loss = gan_loss + (LAMBDA * l1_loss)
+
+    return total_gen_loss, gan_loss, l1_loss
+
+
+def discriminator_loss(disc_real_output, disc_generated_output):
+    real_loss = loss_object(tf.ones_like(disc_real_output), disc_real_output)
+
+    generated_loss = loss_object(tf.zeros_like(
+        disc_generated_output), disc_generated_output)
+
+    total_disc_loss = real_loss + generated_loss
+
+    return total_disc_loss
+
+
+# Optimizadores
+generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+
+
+def generate_images(model, test_input, tar):
+    prediction = model(test_input, training=True)
+    plt.figure(figsize=(15, 15))
+
+    display_list = [test_input[0], tar[0], prediction[0]]
+    title = ['Input Image', 'Ground Truth', 'Predicted Image']
+
+    for i in range(3):
+        plt.subplot(1, 3, i+1)
+        plt.title(title[i])
+        # getting the pixel values between [0, 1] to plot it.
+        plt.imshow(display_list[i] * 0.5 + 0.5)
+        plt.axis('off')
+    plt.show()
+
+
+for example_input, example_target in test_dataset.take(1):
+    generate_images(generator, example_input, example_target)
+
+
+@tf.function
+def train_step(input_image, target, epoch):
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        gen_output = generator(input_image, training=True)
+
+        disc_real_output = discriminator([input_image, target], training=True)
+        disc_generated_output = discriminator(
+            [input_image, gen_output], training=True)
+
+        gen_total_loss, gen_gan_loss, gen_l1_loss = generator_loss(
+            disc_generated_output, gen_output, target)
+        disc_loss = discriminator_loss(disc_real_output, disc_generated_output)
+
+    generator_gradients = gen_tape.gradient(gen_total_loss,
+                                            generator.trainable_variables)
+    discriminator_gradients = disc_tape.gradient(disc_loss,
+                                                 discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(generator_gradients,
+                                            generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(discriminator_gradients,
+                                                discriminator.trainable_variables))
+
+
+def fit(train_ds, epochs, test_ds):
+    for epoch in range(epochs):
+        start = time.time()
+
+        display.clear_output(wait=True)
+
+        for example_input, example_target in test_ds.take(1):
+            generate_images(generator, example_input, example_target)
+        print("Epoch: ", epoch)
+
+        # Train
+        for n, (input_image, target) in train_ds.enumerate():
+            print('.', end='')
+            if (n+1) % 100 == 0:
+                print()
+            train_step(input_image, target, epoch)
+        print()
+
+        # saving (checkpoint) the model every 20 epochs
+        # if (epoch + 1) % 20 == 0:
+        #  checkpoint.save(file_prefix = checkpoint_prefix)
+
+        # print ('Time taken for epoch {} is {} sec\n'.format(epoch + 1,
+        #                                                    time.time()-start))
+
+
+EPOCHS = 150
+fit(train_dataset, EPOCHS, test_dataset)
